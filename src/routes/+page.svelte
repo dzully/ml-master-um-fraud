6bb70fca817f7dca9870ab8d532a8cac807a4dd0
Mohamad Dzul Syakimin <dzulsyakimin61@gmail.com>
2024-04-29 08:35:33 +0800

    Add Dockerfile and .dockerignore, update package.json and postcss.config.js, and modify +page.svelte and layout.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,20 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="global">
+	<div class="p-4 bg-white rounded-lg shadow-lg">
+		<h1 class="text-2xl font-bold">Machine Learning</h1>
+		<p class="mt-2 text-gray-600">Fraud Analysis by Group 1</p>
+	</div>
+</div>
+
+<style lang="postcss">
+	:global(html) {
+		background-color: theme(colors.gray.100);
+	}
+
+	.global {
+		position: fixed;
+		inset: 0;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+</style>
